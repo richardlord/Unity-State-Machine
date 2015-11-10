403b9de3c9b3853f6f7621d4f79ecb9309806434
Richard Lord <richard@richardlord.net>
2015-11-10 09:12:08 +0000

    New example.

---
+++ Assets/Example/States/Slide.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slide : MonoBehaviour
+{
+	public float speed;
+	public float limit;
+
+	void Update ()
+	{
+		Vector3 position = transform.position;
+		position.x += speed * Time.deltaTime;
+		transform.position = position;
+		if( position.x >= limit || position.x <= -limit )
+		{
+			speed = -speed;
+		}
+	}
+}
---
+++ Assets/Example/States/SpriteColourizer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteColourizer : MonoBehaviour
+{
+	public Color color;
+
+	void Start ()
+	{
+		GetComponent<SpriteRenderer>().color = color;
+	}
+}
