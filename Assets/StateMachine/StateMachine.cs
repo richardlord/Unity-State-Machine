1a4390773679fab258aca009850a27541f10752c
Richard Lord <richard@richardlord.net>
2016-09-13 09:46:43 +0100

    Simplify

--- Assets/StateMachine/StateMachine.cs
+++ Assets/StateMachine/StateMachine.cs
@@ -8,11 +8,10 @@ public class StateMachine : MonoBehaviour
 	public string firstState;
 	public StateConfig[] states;
 
-	private ActiveComponentData currentStateData;
+	private MonoBehaviour currentStateComponent;
 	private string currentStateName;
 
-	private Dictionary<string, int> statesDictionary;
-	private Dictionary<int, ComponentData> componentsDictionary;
+	private Dictionary<string, ComponentData> statesDictionary;
 
 	public string currentState
 	{
@@ -24,17 +23,11 @@ public class StateMachine : MonoBehaviour
 
 	void Awake ()
 	{
-		statesDictionary = new Dictionary<string, int>( states.Length );
+		statesDictionary = new Dictionary<string, ComponentData>( states.Length );
 		foreach( StateConfig state in states )
 		{
-			statesDictionary[ state.name ] = state.component.GetInstanceID();
+			statesDictionary[ state.name ] = SerializeComponent( state.component );
 		}
-		componentsDictionary = new Dictionary<int, ComponentData>();
-		foreach( StateConfig state in states )
-		{
-			serializeComponent( state.component );
-		}
-
 		if( firstState != null )
 		{
 			ChangeState( firstState );
@@ -45,51 +38,36 @@ public class StateMachine : MonoBehaviour
 	{
 		if( stateName != currentStateName && statesDictionary.ContainsKey( stateName ) )
 		{
-			if( currentStateData.component != null )
+			if( currentStateComponent != null )
 			{
-				serializeComponent( currentStateData );
+				statesDictionary[ currentStateName ] = SerializeComponent( currentStateComponent );
 			}
 				
 			currentStateName = stateName;
-			currentStateData = deserializeComponent( statesDictionary[ stateName ] );
+			currentStateComponent = DeserializeComponent( statesDictionary[stateName] );
 		}
 	}
 
-	private int[] getComponentIds( MonoBehaviour[] components )
-	{
-		int[] ids = new int[ components.Length ];
-		for( int i = 0; i < components.Length; ++i )
-		{
-			MonoBehaviour component = components[i];
-			ids[i] = component.GetInstanceID();
-		}
-		return ids;
-	}
-
-	private void serializeComponent( MonoBehaviour component )
+	private ComponentData SerializeComponent( MonoBehaviour component )
 	{
 		if( component != null )
 		{
-			componentsDictionary[component.GetInstanceID()] = new ComponentData( component.GetInstanceID(), component.GetType(), JsonUtility.ToJson( component ) );
+			ComponentData componentData = new ComponentData( component.GetType(), JsonUtility.ToJson( component ) );
 			DestroyImmediate( component );
+			return componentData;
 		}
+		return null;
 	}
 
-	private void serializeComponent( ActiveComponentData componentData )
+	private MonoBehaviour DeserializeComponent( ComponentData data )
 	{
-		if( componentData.component != null )
+		if( data != null )
 		{
-			componentsDictionary[componentData.id] = new ComponentData( componentData.id, componentData.component.GetType(), JsonUtility.ToJson( componentData.component ) );
-			DestroyImmediate( componentData.component );
+			MonoBehaviour component = gameObject.AddComponent( data.type ) as MonoBehaviour;
+			JsonUtility.FromJsonOverwrite( data.data, component );
+			return component;
 		}
-	}
-
-	private ActiveComponentData deserializeComponent( int id )
-	{
-		ComponentData data = componentsDictionary[id];
-		MonoBehaviour component = gameObject.AddComponent( data.type ) as MonoBehaviour;
-		JsonUtility.FromJsonOverwrite( data.data, component );
-		return new ActiveComponentData( id, component );
+		return null;
 	}
 
 	[Serializable]
@@ -99,29 +77,15 @@ public class StateMachine : MonoBehaviour
 		public MonoBehaviour component;
 	}
 
-	struct ComponentData
+	class ComponentData
 	{
-		public int id;
 		public Type type;
 		public string data;
 
-		public ComponentData( int id, Type type, string data )
+		public ComponentData( Type type, string data )
 		{
-			this.id = id;
 			this.type = type;
 			this.data = data;
-		}
-	}
-
-	struct ActiveComponentData
-	{
-		public int id;
-		public MonoBehaviour component;
-
-		public ActiveComponentData( int id, MonoBehaviour component )
-		{
-			this.id = id;
-			this.component = component;
 		}
 	}
 }
